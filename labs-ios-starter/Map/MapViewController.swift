85a2fd04cece566c3a02165405bcebf58abb7975
Denis Cedeno <denis.cedeno1@me.com>
2021-01-26 23:12:04 -0500

    Search Bar Implemented in map view, instantiated table view programmatically using id

--- labs-ios-starter/Map/MapViewController.swift
+++ labs-ios-starter/Map/MapViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark: MKPlacemark)
+}
+
+class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
+    var resultSearchController: UISearchController? = nil
+    var selectedPin: MKPlacemark? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +31,28 @@ class MapViewController: UIViewController {
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
+        
+        mapView.delegate = self
+        addSearbarTable()
+    }
+    
+    func addSearbarTable() {
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "locationSearchTable") as! LocationSearchTableVC
+        
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable as UISearchResultsUpdating
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        
+        navigationItem.searchController = resultSearchController
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        definesPresentationContext = true
+        
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
     }
 }
 
@@ -45,5 +73,28 @@ extension MapViewController : CLLocationManagerDelegate {
     
     private func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         print("error:: \(error)")
+    }
+}
+
+extension MapViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark: MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        
+        if let city = placemark.locality,
+           let state = placemark.administrativeArea {
+            annotation.subtitle = "\(city), \(state) \n CitySpire Score: 77"
+        }
+        
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: placemark.coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
     }
 }
