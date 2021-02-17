c35ce89e5d86f26dd066a1d5a6a8a381e3994f8c
Denis Cedeno <denis.cedeno1@me.com>
2021-02-17 14:40:22 -0500

    Moved favorites story board to map story board, favorites collection selection displaying detail view.

--- labs-ios-starter/Map/DetailViewController.swift
+++ labs-ios-starter/Map/DetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import EMTNeumorphicView
 
 class DetailViewController: UIViewController {
-    
+
     // MARK: - Properties
     
     var isFavorite: Bool = false
@@ -19,6 +19,7 @@ class DetailViewController: UIViewController {
     var city: City?
     var zip: String?
     var address: String?
+    
     
     // MARK: - IBOutlets
     
