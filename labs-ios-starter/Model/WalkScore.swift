664d634b1204adcc7a35f48023d136fb5130936a
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2021-02-22 22:04:49 -0600

    Updated Model
    Added attributes to model to be able to take in zipcodes once again to pass on to Data Science

--- labs-ios-starter/Model/WalkScore.swift
+++ labs-ios-starter/Model/WalkScore.swift
@@ -31,3 +31,19 @@ struct Welcome: Codable {
     }
 }
 
+struct zipResults: Decodable {
+    let features: [Text]
+}
+
+class Text: NSObject, Decodable {
+    let zipCode: String
+    
+    enum CodingKeys: String, CodingKey {
+        case text
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.zipCode = try container.decode(String.self, forKey: .text)
+    }
+}
